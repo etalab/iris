0b15eb0ef6ee861031e5ebe3319ddfbf74e19031
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-10-04 17:28:44 +0200

    Wrap server definition in async function

--- server.js
+++ server.js
@@ -7,80 +7,87 @@ const {bbox: getBbox, booleanPointInPolygon, point, lineString, pointToLineDista
 const morgan = require('morgan')
 const cors = require('cors')
 
-const app = express()
+async function main() {
+  const app = express()
 
-const geoIndex = new Flatbush(features.length)
-features.forEach(f => geoIndex.add(...getBbox(f)))
-geoIndex.finish()
+  const geoIndex = new Flatbush(features.length)
+  features.forEach(f => geoIndex.add(...getBbox(f)))
+  geoIndex.finish()
 
-if (process.env.NODE_ENV === 'production') {
-  app.enable('trust proxy')
-}
-
-app.use(cors({origin: true}))
-
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'))
-}
-
-app.get('/iris', (req, res) => {
-  if (!req.query.lat || !req.query.lon || !req.query.codeCommune) {
-    return res.sendStatus(400)
+  if (process.env.NODE_ENV === 'production') {
+    app.enable('trust proxy')
   }
 
-  const lat = Number.parseFloat(req.query.lat)
-  const lon = Number.parseFloat(req.query.lon)
+  app.use(cors({origin: true}))
 
-  if (lat > 90 || lat < -90 || lon > 180 || lon < -180) {
-    return res.sendStatus(400)
+  if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'))
   }
 
-  const candidates = geoIndex.neighbors(lon, lat, 10, 10, undefined)
-    .filter(i => features[i].properties.codeCommune === req.query.codeCommune)
-    .map(i => features[i])
+  app.get('/iris', (req, res) => {
+    if (!req.query.lat || !req.query.lon || !req.query.codeCommune) {
+      return res.sendStatus(400)
+    }
 
-  if (candidates.length === 0) {
-    return res.sendStatus(404)
-  }
+    const lat = Number.parseFloat(req.query.lat)
+    const lon = Number.parseFloat(req.query.lon)
 
-  const exactResult = candidates.find(c => {
-    return booleanPointInPolygon(
-      point([lon, lat]),
-      c
-    )
+    if (lat > 90 || lat < -90 || lon > 180 || lon < -180) {
+      return res.sendStatus(400)
+    }
+
+    const candidates = geoIndex.neighbors(lon, lat, 10, 10, undefined)
+      .filter(i => features[i].properties.codeCommune === req.query.codeCommune)
+      .map(i => features[i])
+
+    if (candidates.length === 0) {
+      return res.sendStatus(404)
+    }
+
+    const exactResult = candidates.find(c => {
+      return booleanPointInPolygon(
+        point([lon, lat]),
+        c
+      )
+    })
+
+    if (exactResult) {
+      return res.send(exactResult.properties)
+    }
+
+    const fuzzyResult = chain(candidates)
+      .minBy(c => {
+        const rings = []
+
+        if (c.geometry.type === 'Polygon') {
+          rings.push(...c.geometry.coordinates)
+        }
+
+        if (c.geometry.type === 'MultiPolygon') {
+          c.geometry.coordinates.forEach(polygonRings => rings.push(...polygonRings))
+        }
+
+        return min(rings.map(ring => {
+          return pointToLineDistance(point([lon, lat]), lineString(ring), {units: 'kilometers'})
+        }))
+      })
+      .value()
+
+    if (fuzzyResult) {
+      return res.send(fuzzyResult.properties)
+    }
+
+    res.sendStatus(404)
   })
 
-  if (exactResult) {
-    return res.send(exactResult.properties)
-  }
+  const port = process.env.PORT || 5000
 
-  const fuzzyResult = chain(candidates)
-    .minBy(c => {
-      const rings = []
+  app.listen(port, () => {
+    console.log(`Start listening on port ${port}`)
+  })
+}
 
-      if (c.geometry.type === 'Polygon') {
-        rings.push(...c.geometry.coordinates)
-      }
-
-      if (c.geometry.type === 'MultiPolygon') {
-        c.geometry.coordinates.forEach(polygonRings => rings.push(...polygonRings))
-      }
-
-      return min(rings.map(ring => {
-        return pointToLineDistance(point([lon, lat]), lineString(ring), {units: 'kilometers'})
-      }))
-    })
-    .value()
-
-  if (fuzzyResult) {
-    return res.send(fuzzyResult.properties)
-  }
-
-  res.sendStatus(404)
-})
-
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {
-  console.log(`Start listening on port ${port}`)
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
 })
